d527103c40a92f579659ab7ed4f48a9f70a7095c
GarethProjects <64733108+GarethProjects@users.noreply.github.com>
2023-02-05 22:50:46 +0000

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "gl-back-end-tf-statefile-2010"
     key = "main"
-    region = "us-east-1"
+    region = "us-east-2"
     dynamodb_table = "terraform-state-backend-table"
   }
 }
