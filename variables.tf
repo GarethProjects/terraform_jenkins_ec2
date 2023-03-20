31df2b6d8287f2330e724b7d94c337847bf0e995
GarethProjects <64733108+GarethProjects@users.noreply.github.com>
2023-03-20 21:20:42 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,13 @@
-# variable "access_key" {
-#   type = string
-# }
-# variable "secret_key" {
-#   type = string
-# }
+# Input Variables
+variable "aws_region" {
+  description = "Region in which AWS resources to be created"
+  type        = string
+  default     = "eu-west-1"
+}
+
+variable "ec2_instance_count" {
+  description = "EC2 Instance Count"
+  type        = number
+  default     = 2
+} 
+  
