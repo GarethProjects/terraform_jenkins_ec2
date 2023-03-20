4d4291bba0671d2e863cd361b242736d9e9aceac
GarethProjects <64733108+GarethProjects@users.noreply.github.com>
2023-03-20 21:23:00 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,13 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "myec2vm" {
   ami           = "ami-0f1a5f5ada0e7da53"
   instance_type = "t2.nano"
   count                  = var.ec2_instance_count
+  tags = {
+    "Name" = "myec2vm"
+  }
 }
+
+
